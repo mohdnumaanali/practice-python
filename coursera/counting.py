9e2f01503d71208697b641655d068942ef7672a2
mohdnumaanali <mohammednumaanalins@gmail.com>
2025-12-23 07:59:59 +0530

    today coursera problem

---
+++ coursera/counting.py
@@ -0,0 +1,19 @@
+# # List of numbers
+# numbers = [9, 41, 12, 3, 74, 15]
+
+# # Initialize the counter
+# count = 0
+
+# # Loop through each number in the list
+# for number in numbers:
+#     count += 1  # Increment the counter for each number
+
+# # Print the total count
+# print("Total count:", count)
+
+zork = 0
+print('Before', zork)
+for thin in [9, 41, 12, 3, 74, 15]:
+    zork = zork + 1
+    print(zork, thin)
+print('After', zork)
