afb4d4960fb90ad7d23c976cddf6fc6a5b5799f0
mohdnumaanali <mohammednumaanalins@gmail.com>
2026-01-31 07:56:38 +0530

    def funtions revive questions

---
+++ coursera/practise_problem/deff(1).py
@@ -0,0 +1,6 @@
+def welcome(name):
+    inp = "hello , " + name
+    return inp
+
+print(welcome("numaan"))
+print(welcome("ali"))
---
+++ coursera/practise_problem/deff(2).py
@@ -0,0 +1,8 @@
+#squareing a number 
+
+def square(n):
+    return n*n
+print(square(3))
+print(square(7))
+print(square(9))
+print(square(100))
